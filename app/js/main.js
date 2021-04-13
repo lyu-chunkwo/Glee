20c06e25e879df4b9f3d3b8d6efc719e8814a979
lyu-chunkwo <lyu.chun.kwo@gmail.com>
2021-04-13 13:39:41 +0600

    fix process

--- app/js/main.js
+++ app/js/main.js
@@ -11,10 +11,29 @@ $(function() {
 		autoplay: true,
 	});
 
+	const mix1 = document.querySelector('.products__list');
+	const mix2 = document.querySelector('.design__list');
 
-  var mixer = mixitup('.products__list');
-  var mixer = mixitup('.design__list');
+	if(mix1){
+		mixitup('.products__list', {
+			selectors: {
+				control: '.filter1'
+			}
+		})
+	}
+
+	if(mix2){
+		mixitup('.design__list', {
+			selectors: {
+				control: '.filter2'
+			}
+		})
+	}
 	
-
+	$(".stars").rateYo({
+		starWidth: "40px",
+		normalFill: "#A0A0A0",
+		ratedFill: "#E74C3C"
+  });
 
 });
